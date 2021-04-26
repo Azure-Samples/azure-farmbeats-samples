18c734411b213ddb2f07a2702203e8997dfb1a5a
Annam Naresh <annaresh@microsoft.com>
2021-04-26 18:44:09 +0530

    removed all farmbeats_config and weather_config details

--- evi_forecast/utils/config.py
+++ evi_forecast/utils/config.py
@@ -1,16 +1,15 @@
 """
 Copyright (c) Microsoft Corporation. All rights reserved.
 FarmBeats config file
-FarmBeats registered farmer details
 """
 
 farmbeats_config = {
-    "client_id": "640027d7-3e96-49fc-8ecf-775f47a1e7b8",
-    "client_secret": "rC.NHz~YPJn_xKGMh-OTg3M4P2kn6EbI90",
+    "client_id": "",
+    "client_secret": "",
     "authority": "https://login.windows-ppe.net",
-    "default_scope": "https://farmbeats-dogfood.azure.net/.default",
-    "instance_url": "https://agadhibjs-one.farmbeats-dogfood.azure.net",
-    "tenant_id": "e21b7e4f-4b6c-4ead-bb26-b615da83f381",
-    "weather_provider_key": "c743a0f0d236e4115aef962a1eed0709",
-    "weather_provider_id": "95615ca0",
+    "default_scope": "",
+    "instance_url": "",
+    "tenant_id": "",
+    "weather_provider_key": "",
+    "weather_provider_id": "",
 }
