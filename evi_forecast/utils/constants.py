7c631d872b61452f1547d62a0789b4bc5c4fad4c
Annam_Naresh <annaresh@microsoft.com>
2021-05-10 17:44:36 +0530

    updated constants file

--- evi_forecast/utils/constants.py
+++ evi_forecast/utils/constants.py
@@ -13,16 +13,16 @@ CONSTANTS= {
     # model specs
     "input_days": 30,  # input number of days for NDVI/EVI and weather
     "output_days": 10,  # number of days of weather forecast
-    "ref_tm_model": "10-05-2020",  # start of growing season
+    "ref_tm_model": "10-08-2020",  # start of growing season
     "sat_res_x_model": 10,  # spatial sampling in model training, this might differ for inferance
-    "interp_date_start": "10-04-2020",  # model iterpolation start date
-    "interp_date_end": "30-11-2020",  # model iterpolation start date
+    "interp_date_start": "10-07-2020",  # model iterpolation start date
+    "interp_date_end": "10-05-2021",  # model iterpolation start date
     
     # model results filenames
     "results_dir": "results/",
     "ardpkl": "results/ARD_18mar2021.pkl",  # Analysis ready dataset pickle file
-    "w_pkl": "results/weather_parms_18mar2021.pkl",  # training data weather parameters list and sttaistics
-    "modelh5": "results/model_100_100_100_18mar2021.h5",  # trained model in h5 format
+    "w_pkl": "model/weather_parms_18mar2021.pkl",  # training data weather parameters list and sttaistics
+    "modelh5": "model/model_100_100_100_18mar2021.h5",  # trained model in h5 format
     "model_result_png": "results/ANN_results_100_100_100_18mar2021.png",  # validation error results
     "var_name": "ndvi",
 }
