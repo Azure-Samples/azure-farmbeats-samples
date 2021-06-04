88f172b7174e599a5396aca0325002b6d60f6cb0
bkuriach <bkuriach@asu.edu>
2021-06-04 11:34:24 +0530

    remove details from config

--- evi_forecast/utils/config.py
+++ evi_forecast/utils/config.py
@@ -2,13 +2,13 @@
 # Licensed under the MIT license.
 
 farmbeats_config = {
-    "client_id": "39d176c5-91b6-454b-854d-55813c17d92d",
-    "client_secret": "G..44gDU7t3_B3PV8~EhxLbY.V1lpQPfU~",
-    "authority": "https://login.microsoftonline.com",
-    "default_scope": "https://farmbeats.azure.net/.default",
-    "instance_url": "https://internal-wcus.farmbeats.azure.net",
-    "tenant_id": "72f988bf-86f1-41af-91ab-2d7cd011db47",
-    "weather_provider_extension_id": "DTN.ClearAg",
-    "weather_provider_key": "c743a0f0d236e4115aef962a1eed0709",
-    "weather_provider_id": "95615ca0",
+    "client_id": "",
+    "client_secret": "",
+    "authority": "",
+    "default_scope": "",
+    "instance_url": "",
+    "tenant_id": "",
+    "weather_provider_extension_id": "",
+    "weather_provider_key": "",
+    "weather_provider_id": "",
 }
